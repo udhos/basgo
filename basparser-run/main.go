768b899b330132ee511c68cd57e7a5fd7dce814e
udhos <udhos>
2019-03-08 16:15:09 -0300

    Clean-up.

--- basparser-run/main.go
+++ basparser-run/main.go
@@ -13,16 +13,6 @@ func main() {
 	me := os.Args[0]
 	log.Printf("%s: reading from stdin...", me)
 
-	/*
-		debug := true
-		input := bufio.NewReader(os.Stdin)
-		log.Printf("%s: input buffer size: %d", me, input.Size())
-		lex := basparser.NewInputLex(input, debug)
-		basparser.Reset()
-		status := basparser.InputParse(lex)
-		nodes := basparser.Result.Root
-	*/
-
 	result, status, errors := basparser.Run(me, os.Stdin)
 
 	nodes := result.Root
@@ -31,8 +21,8 @@ func main() {
 
 	log.Printf("%s: status=%d errors=%d", me, status, errors)
 
-	log.Printf("%s: FOR count=%d NEXT count=%d", me, basparser.Result.CountFor, basparser.Result.CountNext)
-	log.Printf("%s: WHILE count=%d WEND count=%d", me, basparser.Result.CountWhile, basparser.Result.CountWend)
+	log.Printf("%s: FOR count=%d NEXT count=%d", me, result.CountFor, result.CountNext)
+	log.Printf("%s: WHILE count=%d WEND count=%d", me, result.CountWhile, result.CountWend)
 
 	log.Printf("%s: syntax tree lines=%d:", me, len(nodes))
 
