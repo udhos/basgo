d0e4b29f68d4439d17478c0fedae62cf65367873
udhos <udhos>
2019-03-11 17:46:04 -0300

    Colors from RGB.

--- baslib/color_map.go
+++ baslib/color_map.go
@@ -7,29 +7,28 @@ import (
 )
 
 var colorBasToTerm = [16]int{
-	int(tcell.ColorBlack),       // 0
-	int(tcell.ColorDarkBlue),    // 1
-	int(tcell.ColorGreen),       // 2
-	int(tcell.ColorDarkCyan),    // 3
-	int(tcell.ColorRed),         // 4
-	int(tcell.ColorDarkMagenta), // 5
-	int(tcell.ColorSaddleBrown), // 6
-	int(tcell.ColorLightGray),   // 7
-	int(tcell.ColorGray),        // 8
-	int(tcell.ColorBlue),        // 9
-	int(tcell.ColorLightGreen),  // 10
-	int(tcell.ColorLightCyan),   // 11
-	int(tcell.ColorTomato),      // 12
-	int(tcell.ColorPink),        // 13
-	int(tcell.ColorYellow),      // 14
-	int(tcell.ColorWhite),       // 15
+	int(tcell.NewRGBColor(0, 0, 0)),          // 0 black
+	int(tcell.NewRGBColor(0, 0, 0xa8)),       // 1 blue
+	int(tcell.NewRGBColor(0, 0xa8, 0)),       // 2 green
+	int(tcell.NewRGBColor(0, 0xa8, 0xa8)),    // 3 cyan
+	int(tcell.NewRGBColor(0xa8, 0, 0)),       // 4 red
+	int(tcell.NewRGBColor(0xa8, 0, 0xa8)),    // 5 magenta
+	int(tcell.NewRGBColor(0xa8, 0x54, 0)),    // 6 brown (dark yellow)
+	int(tcell.NewRGBColor(0xa8, 0xa8, 0xa8)), // 7 gray (dark white)
+	int(tcell.NewRGBColor(0x54, 0x54, 0x54)), // 8 dark gray (bright black)
+	int(tcell.NewRGBColor(0x54, 0x54, 0xfc)), // 9 bright blue
+	int(tcell.NewRGBColor(0x54, 0xfc, 0x54)), // 10 bright green
+	int(tcell.NewRGBColor(0x54, 0xfc, 0xfc)), // 11 bright cyan
+	int(tcell.NewRGBColor(0xfc, 0x54, 0x54)), // 12 bright red
+	int(tcell.NewRGBColor(0xfc, 0x54, 0xfc)), // 13 bright magenta (pink)
+	int(tcell.NewRGBColor(0xfc, 0xfc, 0x54)), // 14 yellow
+	int(tcell.NewRGBColor(0xfc, 0xfc, 0xfc)), // 15 white
 }
 
 var colorTermToBas map[int]int
 
 func init() {
 	colorTermToBas = map[int]int{}
-
 	for i, v := range colorBasToTerm {
 		colorTermToBas[v] = i
 	}
