a3df0711f5d79e287ff1a58658f05c450691c566
udhos <udhos>
2019-08-18 14:16:46 -0300

    2 SwapBuffers

--- baslib/graphics.go
+++ baslib/graphics.go
@@ -242,7 +242,8 @@ func draw(mode, vao uint32, window *glfw.Window, count int32) {
 		gl.BindVertexArray(vao)
 		gl.DrawArrays(mode, 0, count)
 
-		//window.SwapBuffers()
+		window.SwapBuffers()
+		window.SwapBuffers()
 	})
 }
 
